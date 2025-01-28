2ddbf7459ccd9690e92d82c500acf44743d98835
unknown <blahayacine9@gmail.com>
2025-01-28 01:19:49 +0100

    s

--- dist/app.js
+++ dist/app.js
@@ -1,55 +1,17 @@
 "use strict";
-class Department {
-    constructor(id, name) {
-        this.id = id;
-        this.name = name;
-        this.employees = [];
-    }
-    describe() {
-        console.log(`Department (${this.id}): ${this.name}`);
-    }
-    addEmployee(employee) {
-        this.employees.push(employee);
-    }
-    printEmployeeInformation() {
-        console.log(this.employees.length);
-        console.log(this.employees);
-    }
-}
-class ITDepartment extends Department {
-    constructor(id, admins) {
-        super(id, 'IT');
-        this.admins = admins;
-    }
-}
-class AccountingDepartment extends Department {
-    get getMostRecentReport() {
-        if (this.lastReport) {
-            return this.lastReport;
-        }
-        throw new Error('no report available');
-    }
-    set setMostRecentReport(report) {
-        if (!report) {
-            throw new Error("empty report dog");
-        }
-        this.addReport(report);
-    }
-    constructor(id, reports) {
-        super(id, 'Accounting');
-        this.reports = reports;
-        this.lastReport = reports[0];
-    }
-    addReport(text) {
-        this.reports.push(text);
-        this.lastReport = text;
-    }
-    printReports() {
-        console.log(this.reports);
-    }
-}
-const it = new ITDepartment('d1', ['Max']);
-const accounting = new AccountingDepartment('d2', []);
-const reprttt = '';
-accounting.setMostRecentReport = reprttt;
-console.log(accounting.getMostRecentReport);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const todos_1 = __importDefault(require("./router/todos"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use('/', todos_1.default);
+app.use((error, req, res, next) => {
+    res.status(500).json({ message: error.message });
+});
+// Start the server
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,15 @@
-// type AddFn = (a: number, b: number) => number;
-interface AddFn {
-  (a: number, b: number): number;
-}
+import express, { Request, Response, NextFunction } from 'express';
+import todosRouter from './router/todos';
 
-let add: AddFn;
+const app = express();
 
-add = (n1: number, n2: number) => {
-  return n1 + n2;
-};
+app.use(express.json());
+app.use('/', todosRouter);
+app.use((error:Error,req: Request, res: Response, next: NextFunction) => {
+    res.status(500).json({message:error.message});
+});
 
-interface Named {
-  readonly name?: string;
-  outputName?: string;
-}
-
-interface Greetable extends Named {
-  greet(phrase: string): void;
-}
-
-class Person implements Greetable {
-  name?: string;
-  age = 30;
-
-  constructor(n?: string) {
-    if (n) {
-      this.name = n;
-    }
-  }
-
-  greet(phrase: string) {
-    if (this.name) {
-      console.log(phrase + ' ' + this.name);
-    } else {
-      console.log('Hi!');
-    }
-  }
-}
-
-let user1: Greetable;
-
-user1 = new Person();
-// user1.name = 'Manu';
-
-user1.greet('Hi there - I am');
-console.log(user1);
+// Start the server
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
